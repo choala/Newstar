acc4df333901578d8ba91e8e6fa18261b8a28b27
kempt007 <133959724+kempt007@users.noreply.github.com>
2023-08-21 09:57:15 +0900

    image 추가

---
+++ Newstar/Newstar/CategoryDetailView.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryDetailView.swift
+//  Newstar
+//
+//  Created by 윤경환 on 2023/08/17.
+//
+
+import SwiftUI
+
+struct CategoryDetailView: View {
+    // MARK: - PROPERTIY
+    var uiImageView = UIImageView()
+    
+    // MARK: - BODY
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Hello")
+            }
+        }
+    }
+}
+
+struct CategoryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryDetailView()
+    }
+}
